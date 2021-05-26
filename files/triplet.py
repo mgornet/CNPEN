febff287322ca1c3e36fe1b9bc1cf298b40a7575
mgornet <melanie.gornet@gmail.com>
2021-05-26 12:04:16 +0200

    add TripletLossRaw

--- files/triplet.py
+++ files/triplet.py
@@ -7,7 +7,6 @@ from torchvision import transforms
 # LOSS
 #################################################################################
 
-# TO DO Loss for testing without mean
 class TripletLoss(nn.Module):
 
     def __init__(self, margin):
@@ -19,6 +18,19 @@ class TripletLoss(nn.Module):
         distance_negative = (anchor - negative).pow(2).sum(1) 
         losses = F.relu(distance_positive - distance_negative + self.margin)
         return losses.mean() if size_average else losses.sum()
+
+
+class TripletLossRaw(nn.Module):
+
+    def __init__(self, margin):
+        super(TripletLossRaw, self).__init__()
+        self.margin = margin
+
+    def forward(self, anchor, positive, negative, size_average=True):
+        distance_positive = (anchor - positive).pow(2).sum(1) 
+        distance_negative = (anchor - negative).pow(2).sum(1) 
+        losses = F.relu(distance_positive - distance_negative + self.margin)
+        return losses
 
 
 # TRIPLET GENERATOR
