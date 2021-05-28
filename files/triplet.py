54415c4ff0917ec6dd17c153e78ac82f5ac401b2
mgornet <melanie.gornet@gmail.com>
2021-05-28 11:28:17 +0200

    Revert "fix indentation"
    This reverts commit cae26c9f0240051bd096a8a3a42a01499e3093e1.

--- files/triplet.py
+++ files/triplet.py
@@ -7,6 +7,7 @@ from torchvision import transforms
 # LOSS
 #################################################################################
 
+# TO DO Loss for testing without mean
 class TripletLoss(nn.Module):
 
     def __init__(self, margin):
@@ -20,24 +21,11 @@ class TripletLoss(nn.Module):
         return losses.mean() if size_average else losses.sum()
 
 
-class TripletLossRaw(nn.Module):
-
-    def __init__(self, margin):
-        super(TripletLossRaw, self).__init__()
-        self.margin = margin
-
-    def forward(self, anchor, positive, negative, size_average=True):
-        distance_positive = (anchor - positive).pow(2).sum(1) 
-        distance_negative = (anchor - negative).pow(2).sum(1) 
-        losses = F.relu(distance_positive - distance_negative + self.margin)
-        return losses
-
-
 # TRIPLET GENERATOR
 #################################################################################
 
 class TripletGenerator(nn.Module):
-    def __init__(self, Xa_train, Xp_train, batch_size, all_imgs, neg_imgs_idx, transform=False):
+    def __init__(self, Xa_train, Xp_train, batch_size, all_imgs, neg_imgs_idx):
         self.cur_img_index = 0
         self.cur_img_pos_index = 0
         self.batch_size = batch_size
@@ -48,50 +36,39 @@ class TripletGenerator(nn.Module):
         self.cur_train_index = 0
         self.num_samples = Xa_train.shape[0]
         self.neg_imgs_idx = neg_imgs_idx
-
-        self.transform = transform
         
     def __len__(self):
         return self.num_samples // self.batch_size
         
     def __getitem__(self, batch_index):
 
-    	if self.transform :
-            image_transforms = transforms.Compose(
-                  [
-                      transforms.ToTensor(),
-                      transforms.RandomHorizontalFlip(p=0.5)
-                  ]
-              )
+        image_transforms = transforms.Compose(
+              [
+                  transforms.ToTensor(),
+              ]
+          )
+         
+        low_index = batch_index * self.batch_size
+        high_index = (batch_index + 1) * self.batch_size
 
-    	else :
-            image_transforms = transforms.Compose(
-                  [
-                      transforms.ToTensor(),
-                  ]
-              )
+        imgs_a = self.Xa[low_index:high_index]  # Anchors
+        imgs_p = self.Xp[low_index:high_index]  # Positives
+        imgs_n = random.sample(self.neg_imgs_idx, imgs_a.shape[0])  # Negatives
 
-    	low_index = batch_index * self.batch_size
-    	high_index = (batch_index + 1) * self.batch_size
+        imgs_a = self.imgs[imgs_a]
+        imgs_p = self.imgs[imgs_p]
+        imgs_n = self.imgs[imgs_n]
 
-    	imgs_a = self.Xa[low_index:high_index]  # Anchors
-    	imgs_p = self.Xp[low_index:high_index]  # Positives
-    	imgs_n = random.sample(self.neg_imgs_idx, imgs_a.shape[0])  # Negatives
+        anchors = torch.zeros((self.batch_size,3,60,60))
+        positives = torch.zeros((self.batch_size,3,60,60))
+        negatives = torch.zeros((self.batch_size,3,60,60))
 
-    	imgs_a = self.imgs[imgs_a]
-    	imgs_p = self.imgs[imgs_p]
-    	imgs_n = self.imgs[imgs_n]
-
-    	anchors = torch.zeros((self.batch_size,3,60,60))
-    	positives = torch.zeros((self.batch_size,3,60,60))
-    	negatives = torch.zeros((self.batch_size,3,60,60))
-
-    	for batch in range(self.batch_size):
-    		anchors[batch]=image_transforms(imgs_a[batch])
-    		positives[batch]=image_transforms(imgs_p[batch])
-    		negatives[batch]=image_transforms(imgs_n[batch])
+        for batch in range(self.batch_size):
+          anchors[batch]=image_transforms(imgs_a[batch])
+          positives[batch]=image_transforms(imgs_p[batch])
+          negatives[batch]=image_transforms(imgs_n[batch])
             
-    	return (anchors, positives, negatives)
+        return (anchors, positives, negatives)
     
     
 # NETWORK
