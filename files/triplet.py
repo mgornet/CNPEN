24892097e161a10f77e78054a278add5ea1eb6e4
mgornet <71757356+mgornet@users.noreply.github.com>
2021-08-06 16:46:17 +0200

    fix while 2

--- files/triplet.py
+++ files/triplet.py
@@ -257,7 +257,7 @@ class TripletGenerator(nn.Module):
             # random img among all
             xn = random.choices(self.df.index)[0]
             # TO DO check xn classid diff from xa classid if not try again
-            while self.df.Classid[df.index==xn] == self.df.Classid[df.index==xa]:
+            while self.df.Classid[self.df.index==xn] == self.df.Classid[self.df.index==xa]:
                 xn = random.choices(self.df.index)[0]
             Xn.append(xn)
 
