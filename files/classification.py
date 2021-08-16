5c9eb7fd4dc1f5c1302cee4f3f312963344bbd26
mgornet <71757356+mgornet@users.noreply.github.com>
2021-08-16 18:35:58 +0200

    add triplet accuracy

--- files/classification.py
+++ files/classification.py
@@ -2,10 +2,8 @@ import numpy as np
 from tqdm.notebook import tqdm
 import time
 import torch
-from triplet import distance
+from triplet import distance, distance_vectors
 
-
-# PATH = "./CNPEN/files/"
 
 # TESTING LOOP
 ###############################################################################
@@ -47,3 +45,35 @@ def predict(loader, device, model, threshold):
             pred_neg.append(bin_neg)
     
     return pred_pos, pred_neg
+
+def triplet_acc(loader, device, model):
+
+    count_satisfy_condition=0
+    total_count=0
+
+    for step, (anchor_img, positive_img, negative_img) in \
+    enumerate(tqdm(loader, desc="Processing", leave=False)):
+
+        anchor_img = anchor_img.to(device)
+        positive_img = positive_img.to(device)
+        negative_img = negative_img.to(device)
+
+        anchor_out = model(anchor_img)
+        positive_out = model(positive_img)
+        negative_out = model(negative_img)
+
+        for i in range(len(anchor_out)):
+
+            pos_dist = distance_vectors(
+                anchor_out[i],positive_out[i]
+            ).cpu().detach().item()
+            neg_dist = distance_vectors(
+                anchor_out[i],negative_out[i]
+            ).cpu().detach().item()
+
+            if pos_dist < neg_dist:
+                count_satisfy_condition += 1
+
+            total_count += 1
+
+    return count_satisfy_condition/total_count
